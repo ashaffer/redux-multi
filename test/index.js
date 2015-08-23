75b1786978bf04f2411838ee92622c6e75711934
Andrew Shaffer <darawk@gmail.com>
2015-08-23 13:22:42 -0700

    add test

--- test/index.js
+++ test/index.js
@@ -3,12 +3,13 @@
  */
 
 import test from 'tape'
-import redux-multi from '..'
+import multi from '..'
 
 /**
  * Tests
  */
 
-test('should work', () => {
-
+test('should work', ({plan, pass, end}) => {
+  plan(2)
+  multi({dispatch: () => pass('pass')})(() => {})([1, 2])
 })
